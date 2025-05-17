f8e2f395bab99d166511bf5c65d05f34fb0daaa9
Jay Patel <jay_patel_123@hotmail.co.uk>
2025-05-17 22:47:41 +0100

    add a function for dark mode

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+ function darkMode(){
+   document.body.style.background="--clr-accent2"; 
+   document.querySelectorAll('h1').forEach(e=>e.style.color="white");
+   document.querySelectorAll('p').forEach(e=>e.style.color="white");
+  } 
+
+  function lightMode(){
+    document.body.style.background="--clr-accent"; 
+    document.querySelectorAll('h1').forEach(e=>e.style.color="black");
+    document.querySelectorAll('p').forEach(e=>e.style.color="black");
+   }
